8fa8f6be28a9705f953b1d10fbd8f5b8ae272896
CrownKira <t.karwi@yahoo.com>
2021-01-07 22:17:52 +0800

    Clean up gemfile

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::AuthenticationController < ApiController
   skip_before_action :authenticate_user!, only: [:create]
 
   def create
+    # create a token
     user = User.find_by(email: params[:email])
     puts user
     if user&.valid_password?(params[:password])
